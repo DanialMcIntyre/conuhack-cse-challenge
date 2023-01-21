1275017845c837ba6a2ba28214df44255910b99c
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2023-01-21 14:17:18 -0500

    Calculated time it takes with brute force method

--- cse-password-project/App.js
+++ cse-password-project/App.js
@@ -2,9 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  let pass = "testingpass";
+
+  let possibleChars = 92;
+  let length = pass.length;
+  let keysPerSecond = 1000000000;
+
+  let seconds = Math.pow(possibleChars, length) / keysPerSecond;
+
+  let minutes = Math.floor(seconds / 60)
+  seconds = seconds % 60
+
+  let hours = Math.floor(minutes / 60)
+  minutes = minutes % 60
+
+  let days = Math.floor(hours / 24)
+  hours = hours % 24
+
+  let months = Math.floor(days / 30);
+  days = days % 30
+
+  let years = Math.floor(months / 12)
+  months = months % 12
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Years: {years}, Months: {months}, Days: {days}, Hours: {hours}, Minutes: {minutes}, Seconds: {seconds}</Text>
       <StatusBar style="auto" />
     </View>
   );
