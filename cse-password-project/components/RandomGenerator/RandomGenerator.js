3dc3caf7cd38952182be3b1ef331b51b4a67573e
DaoTimothy <79426229+DaoTimothy@users.noreply.github.com>
2023-01-21 16:26:07 -0500

    imported common english words

---
+++ cse-password-project/components/RandomGenerator/RandomGenerator.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+function generateRandomPassword(passwordLength) {
+    let symbols = [["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+                  , ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+                  , ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+                  , ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", ":", ";", ".", ">", ",", "<"]];
+    let password = "";
+    while(password.length < passwordLength) {
+      let random1 = Math.random() * 4;
+      let random2 = Math.random();
+      if (random1 < 2) {
+        random2 *= 26;
+      } else if (random1 < 3) {
+        random2 *= 10;
+      } else {
+        random2 *= 20;
+      }
+      password += symbols[Math.floor(random1)][Math.floor(random2)];
+    }
+    return password;
+  }
+  
+  export default function RandomGenerator() {
+    const [result, setResult] = useState();
+    const regen = () =>{
+          setResult(generateRandomPassword(10));
+      }
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button onPress={() => regen()} title="generate"></Button>
+        <Text>{result}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
--- cse-password-project/pandareader.py
+++
@@ -1,5 +0,0 @@
-import pandas 
-def importEnglishDictionary():
-    df = pandas.read_excel(r'englishwords.xlsx')
-    df = df.values.tolist()
-    return df
