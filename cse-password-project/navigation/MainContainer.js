892107e6d1ef5fdc373b599167a0973bdf66ef9a
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2023-01-22 05:51:27 -0500

    Got rid of home and unnecessary screens

--- cse-password-project/navigation/MainContainer.js
+++ cse-password-project/navigation/MainContainer.js
@@ -4,11 +4,9 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import IonIcons from 'react-native-vector-icons/Ionicons';
 
-import HomeScreen from './screens/HomeScreen'
 import Rater from '../components/Rater/Rater';
 import Generator from '../components/Generator/Generator';
 
-const homeName = "Home";
 const raterName = "Rater";
 const generatorName = "Generator";
 
@@ -18,15 +16,13 @@ export default function MainContainer() {
     return(
         <NavigationContainer>
             <Tab.Navigator
-            initialRouteName={homeName}
+            initialRouteName={raterName}
             screenOptions={({route}) => ({
                 tabBarIcon: ({focused, color, size}) => {
                     let iconName;
                     let rn = route.name;
 
-                    if(rn === homeName) {
-                        iconName = focused ? 'home' : 'home-outline'
-                    } else if (rn === raterName) {
+                    if (rn === raterName) {
                         iconName = focused ? 'list' : 'list-outline'
                     } else if (rn === generatorName) {
                         iconName = focused ? 'settings' : 'settings-outline';
@@ -36,7 +32,6 @@ export default function MainContainer() {
                 }
             })}>
 
-            <Tab.Screen name={homeName} component={HomeScreen}/>
             <Tab.Screen name={raterName} component={Rater}/>
             <Tab.Screen name={generatorName} component={Generator}/>
 
